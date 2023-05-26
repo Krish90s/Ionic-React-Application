d592baecaa898fd4654e4c6a749b537d32976a85
Krish90s <krishnanvenkat4@gmail.com>
2023-05-26 07:47:32 +0530

    stop scan added

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -5,17 +5,29 @@ interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   const scanQRCode = async () => {
+    await BarcodeScanner.checkPermission({ force: true });
+
+    // make background of WebView transparent
+    // note: if you are using ionic this might not be enough, check below
+    BarcodeScanner.hideBackground();
+
     const result = await BarcodeScanner.startScan();
 
     if (result.hasContent) {
-      // Handle the scanned QR code data
       console.log("QR code data:", result.content);
     }
   };
+
+  const stopScan = () => {
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+  };
+
   return (
     <div className="container">
       <strong>Ready to create an app?</strong>
       <IonButton onClick={scanQRCode}>Click Here To Scan</IonButton>
+      <IonButton onClick={stopScan}>Stop Scan</IonButton>
     </div>
   );
 };
