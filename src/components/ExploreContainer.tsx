ec33a5428785b45815ba857b7be34d712b38b777
Krish90s <krishnanvenkat4@gmail.com>
2023-05-26 11:00:56 +0530

    qr scanner added

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,21 @@
-import './ExploreContainer.css';
-
-interface ContainerProps { }
+import { IonButton } from "@ionic/react";
+import "./ExploreContainer.css";
+import { BarcodeScanner } from "@capacitor-community/barcode-scanner";
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const scanQRCode = async () => {
+    const result = await BarcodeScanner.startScan();
+
+    if (result.hasContent) {
+      // Handle the scanned QR code data
+      console.log("QR code data:", result.content);
+    }
+  };
   return (
     <div className="container">
       <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonButton onClick={scanQRCode}>Click Here To Scan</IonButton>
     </div>
   );
 };
